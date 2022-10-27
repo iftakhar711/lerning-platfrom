ba99aeeb948f5829d8dcce57471273604a83188d
iftakhar711 <iftakharudiin80@gmail.com>
2022-10-27 22:05:10 +0600

    privateroure finish

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { AuthContext } from '../context/Context';
 
 const PrivateRoute = ({ children }) => {
     const { user } = useContext(AuthContext)
+    const location = useLocation()
     if (user && user.uid) {
         return children
     }
+    return <>
+        return <Navigate to='/login' state={{ from: location }} replace />
+        {toast.error('login first')}
+    </>
 };
 
 export default PrivateRoute;
