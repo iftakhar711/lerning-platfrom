87e80d0679eb122123d6daf5c24ccab4faef4ebd
iftakhar711 <iftakharudiin80@gmail.com>
2022-10-27 22:39:38 +0600

    finish

--- src/Componenets/Detail.js
+++ src/Componenets/Detail.js
@@ -11,7 +11,7 @@ const Detail = () => {
                 <div className=' flex flex-col mx-auto text-center bg-gray-300 p-4 mt-4 w-10/12 '>
                     <p className=' text-amber-700 font-bold '>{detail.title}</p>
                     <p className=' text-slate-900 mt-2'>{detail.description}</p>
-                    <p className=' text-red-800 font-bold mt-2'>Rating: {detail.rating.rate}</p>
+                    <p className=' text-red-800 font-bold mt-2'>Ratings: {detail.rating.rate}</p>
                 </div>
                 <div className=' flex justify-center items-center'>
                     <Link to='/premium'><button className=' bg-violet-700 p-2 rounded-md text-slate-200 mt-4 hover:bg-blue-500'>Get Premium Access</button></Link>
